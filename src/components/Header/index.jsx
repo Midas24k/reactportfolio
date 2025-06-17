9e1ba61b56ed8a95dd1fd3442f880369ae544e22
Rod Hughey <137853877+Midas24k@users.noreply.github.com>
2025-06-16 23:13:00 -0400

    Add inline headerStyle for Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,21 @@
 import "../styles/Header.css";
 
 const styles = {
-  
+  headerStyle: {
+    backgroundImage:
+      "linear-gradient(to top, #212529, #26292d, #2a2d31, #2f3236, #34363a, #434549, #525558, #626568, #838688, #a6a8aa, #cacccd, #f0f1f1)",
+    display: "flex",
+    justifyContent: "center",
+    fontFamily: "monospace",
+  },
+
   headingStyle: {
     fontSize: "100px",
     color: "silver",
     textAlign: "center",
     padding: "20px",
-    margin: "0", 
-    textShadow: "1px 1px 2px black"
+    margin: "0",
+    textShadow: "1px 1px 2px black",
   },
 };
 
