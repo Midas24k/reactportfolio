f39542b3f3f8a5af7fb41376eb603fcd581a5dde
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2025-06-24 23:00:53 -0400

    Removed row and col

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,6 +1,4 @@
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
